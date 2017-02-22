14f4e8ceaff83f8272230dec1fb1767cc21a900c
Michal Bojanowski <michal2992@gmail.com>
2017-02-22 03:03:24 +0100

    Rudimentary test for lspline()

--- tests/testthat/test-lspline.R
+++ tests/testthat/test-lspline.R
@@ -1,12 +1,14 @@
-if(FALSE) {
-  set.seed(666)
-  d <- data.frame(
-    x = 1:10,
-    y = c(5:1, 2:6) + rnorm(10, 0, 0.5)
-  )
+context("Testing lspline()")
 
+# Data
+set.seed(666)
+d <- data.frame(
+  x = 1:10,
+  y = c(5:1, 2:6)
+)
+
+
+test_that("it works", {
   m1 <- lm(y ~ lspline(x, c(5), marginal=TRUE), data=d)
   m2 <- lm(y ~ lspline(x, c(5), marginal=FALSE), data=d)
-
-
-}
+})
