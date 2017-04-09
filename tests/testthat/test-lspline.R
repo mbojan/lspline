bd33735acc3a511903795c804fed071f97c85214
Michal Bojanowski <michal2992@gmail.com>
2017-04-09 17:30:23 +0200

    Testing predict on model with lspline

--- tests/testthat/test-lspline.R
+++ tests/testthat/test-lspline.R
@@ -1,7 +1,6 @@
-context("Testing lspline()")
+context("lspline() correct coefficients")
 
 # Data
-set.seed(666)
 d <- data.frame(
   x = 1:10,
   y = c(5:1, 2:6)
@@ -23,3 +22,36 @@ test_that("it works for marginal=FALSE", {
   expect_equivalent(coef(m2), c(6, -1, 1) )
 })
 
+
+test_that("fitted values are identical whatever the `marginal`", {
+  expect_identical( fitted(m1), fitted(m2) )
+})
+
+
+
+
+
+context("lspline() predicting")
+
+
+
+test_that("in-data predictions are correct", {
+  expect_equvalent(
+    predict(m1, data.frame(x=5:10)),
+    1:6
+  )
+  expect_equivalent(
+    predict(m1, data.frame(x=1:5)),
+    5:1
+  )
+  expect_equvalent(
+    predict(m2, data.frame(x=5:10)),
+    1:6
+  )
+  expect_equivalent(
+    predict(m2, data.frame(x=1:5)),
+    5:1
+  )
+
+
+})
